62d6c32c71a084b43a3c474f3281fff76533c11b
raditya224 <rya7285@gmail.com>
2026-03-25 11:56:34 +0000

    Script for automated Docker installation

--- docker.sh
+++ docker.sh
@@ -1,13 +1,13 @@
 # script otomatis untuk instalasi docker pada ubuntu 22.04
 
-# Add Docker's official GPG key:
+# Add Docker's official GPG key :
 sudo apt update
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the repository to Apt sources :
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
